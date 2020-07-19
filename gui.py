68bebaea2c46c643456b99939b1e8e3fdbb4d31c
cmustat <67121359+cmustat@users.noreply.github.com>
2020-07-19 10:47:06 -0400

    Add files via upload
    Added following functionality:
    can import paragraph from file 
    can cycle through paragraphs using previous and next
    
    Known Issue: when a new paragraph is loaded into the QtextEdit, the old text is not cleared. In order to clear it you need to highlight all the text in the box first.

--- gui.py
+++ gui.py
@@ -8,6 +8,43 @@ from PyQt5.QtCore import Qt
 #from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot, Qt
 #path.exists('../data/dataset.json')
 
+#Class to define the LL-like traversal through the paragraphs
+class P_Data:
+    def __init__(self,path='covid_chatbot_data.txt'):
+        self.curpos = 0
+        self.data = []
+        with open(path,'r') as f_open:
+	        for line in f_open.readlines():
+		        if line[0] != '#' and line[0] != '\n':
+			        self.data.append(line)
+        self.len = len(self.data)
+        
+
+    #Sets curpos to next   
+    def next(self):
+        newpos = self.curpos+1
+        #If newpos is over the limit, circle to front of list
+        if (newpos >(self.len-1)):
+            newpos = newpos - self.len
+        self.curpos = newpos
+        
+                       
+
+    #Sets curpos to prev 
+    def prev(self):
+        newpos = self.curpos-1
+        #If newpos is under the limit, circle to end of list
+        if (newpos <0):
+            newpos = self.len + newpos
+        self.curpos = newpos
+        
+
+    def get_curpos(self):
+        return self.curpos
+
+    def get_cur_paragraph(self):
+        print(self.data[self.curpos])
+        return self.data[self.curpos]    
 
 
 class Notepad(QWidget):
@@ -18,6 +55,9 @@ class Notepad(QWidget):
         self.end = 0
 
         self.text = QTextEdit(self)
+        self.text.setReadOnly(True)
+        #Set text to first Paragraph
+        self.text.setText(p_data.get_cur_paragraph())
         self.text.selectionChanged.connect(self.handle_selection_changed)
         self.question = QTextEdit(self)
         #self.clr_btn = QPushButton('Clear')
@@ -26,12 +66,21 @@ class Notepad(QWidget):
         self.saveqa_btn = QPushButton('Save QA')
         self.saveqa_btn.setEnabled(False)
         self.text_lim = QLabel("Selection start: 0 end: 0")
+        #Current Paragraph Title
+        #self.p_title =  QLabel("No Paragraph Selected")
+        #self.p_title.setAlignment(Qt.AlignCenter)
+        #self.p_title.setTextFormat(Qt.PlainText)
+        #Intialize GUI
         self.init_ui()
 
+    #Set layout of window
     def init_ui(self):
+        #titlelayout = QVBoxLayout()
         outerlayout = QVBoxLayout()
         firstlayout = QHBoxLayout()
         secondlayout = QHBoxLayout()
+
+        #titlelayout.addWidget(self.p_title)
 
         firstlayout.addWidget(self.prev_btn)
         firstlayout.addWidget(self.text)
@@ -44,10 +93,16 @@ class Notepad(QWidget):
         secondlayout.addWidget(self.text_lim)
         secondlayout.addWidget(self.saveqa_btn)
 
+        #outerlayout.addLayout(titlelayout)
         outerlayout.addLayout(firstlayout)
         outerlayout.addLayout(secondlayout)
         self.setLayout(outerlayout)
         self.setWindowTitle('Save QA Pairs')
+
+        #Setting Actions for Buttons
+        self.prev_btn.clicked.connect(self.prev_on_click)
+        self.next_btn.clicked.connect(self.next_on_click) 
+
         self.show()
 
     def handle_selection_changed(self):
@@ -67,8 +122,27 @@ class Notepad(QWidget):
         with open('test.txt', 'w') as f:
             my_text = self.text.toPlainText()
             f.write(my_text)
+    
+    
+    def next_on_click(self):
+        ##Get next paragraph and show it in text
+        p_data.next()
+        next_paragraph = p_data.get_cur_paragraph()
+        self.text.setPlainText(next_paragraph)
+        
+        
+    
+    def prev_on_click(self):
+        ##Get prev paragraph and show it in text    
+        self.text.clear() 
+        p_data.prev()
+        next_paragraph = p_data.get_cur_paragraph()
+        print(next_paragraph)
+        self.text.setPlainText(next_paragraph)  
 
 if __name__ == '__main__':
+    
+    p_data = P_Data()
     app = QApplication(sys.argv)
     #window = MainWindow()
     #window.show()
